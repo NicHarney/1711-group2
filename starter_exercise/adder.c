cfacaaac565dfd0a1b64b3eb0100621e18fe5743
NicHarney <146464438+NicHarney@users.noreply.github.com>
2023-10-12 14:23:22 +0000

    starter exercise

---
+++ starter_exercise/adder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    float num1,num2;
+
+    printf("Enter the first number: ");
+    scanf("%f",&num1);
+
+    printf("Enter the second number: ");
+    scanf("%f",&num2);
+
+    
+    printf("The sum of the numbers is %g ",num1 + num2);
+
+    return 0;
+
+}
---
+++ starter_exercise/animals.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    char animals[15];
+
+    printf("Enter your favourite animal: ");
+    scanf("%s",animals);
+
+    printf("Your favourite animal is %s\n",animals);
+
+    return 0;
+
+}
