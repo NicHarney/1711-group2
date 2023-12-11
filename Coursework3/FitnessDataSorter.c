69051a8b63051e9e8452be3251e53016dcb16f25
NicHarney <146464438+NicHarney@users.noreply.github.com>
2023-12-11 12:19:40 +0000

    Coursework3

--- Coursework3/FitnessDataSorter.c
+++ Coursework3/FitnessDataSorter.c
@@ -26,27 +26,31 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, char *
     }
 }
 
-
+//function to tell the qsort function how to order the fitness array
 int compare(const void* num1, const void* num2){
 
     const FitnessData *a = (const FitnessData *) num1;
     const FitnessData *b = (const FitnessData *) num2;
     
-
+    //as steps needs to be in descending order, if a > b then don't swap
     if (a->steps > b->steps){
         return -1;
     
     }
-
+    
+    //if a < b then swap
     else if (a->steps < b->steps){
         return 1;
     }
+
+    //don't swap if they are the same
     return 0;
 }
 
 
 int main() {
     
+    //intialise variables
     FitnessData fitness[1000];
     int buffer_size = 1000;
     char line_buffer[buffer_size];
@@ -56,22 +60,27 @@ int main() {
     char steps[100];
     int line_number = 0;
     int i = 0;
-    
-    printf("Enter the file name: ");
+    char new_file[] = ".tsv";
+
+    //gets users file input
+    printf("Enter Filename: ");
 
     fgets(line_buffer, buffer_size, stdin);
     sscanf(line_buffer, " %s ", filename);
     
+   
+
+
+    //opens the file
 	FILE *file = fopen(filename,"r");
     if (file == NULL){
         printf("Error opening file\n");
         return 1;
         
     }
-    else{
-        printf("File successfully loaded.\n");
-    }
+    
 
+    //writes file information to fitness struct
 	while (fgets(line_buffer,buffer_size,file)){
 
         tokeniseRecord(line_buffer,',',date,time,steps);
@@ -85,14 +94,24 @@ int main() {
         line_number++;
     }
     
-    
+    //built in function used to sort arrays
     qsort(fitness,line_number,sizeof(FitnessData),compare);
 
-    for(int i = 0; i<line_number;i++){
-
-        printf("%s/%s/%d\n",fitness[i].date,fitness[i].time,fitness[i].steps);
-    }
+    
     
     fclose(file);
 
+    //concatenate strings to createa new file
+    strcat(filename,new_file);
+
+    //create a new write file 
+    FILE *file2 = fopen(filename,"w");
+
+    //write each element of fitness to the new file
+    for(int i = 0; i < line_number;i++){
+        fprintf(file2, "%s  %s  %d\n", fitness[i].date,fitness[i].time,fitness[i].steps);
+    }
+    fclose(file2);
+
+    printf("Data sorted and written to %s\n",filename);
 }
